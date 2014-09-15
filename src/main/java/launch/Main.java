264db5743b6193366f2bc124aa7fc1a9c5bc5f33
max-dev <kharchenko.max@gmail.com>
2014-09-15 14:21:12 +0300

    Preparing for Heroku deployment

--- src/main/java/launch/Main.java
+++ src/main/java/launch/Main.java
@@ -11,7 +11,6 @@ import org.apache.catalina.startup.Tomcat;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-
         String webappDirLocation = "src/main/webapp/";
         Tomcat tomcat = new Tomcat();
 
